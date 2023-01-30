2353b3bb9f3a61082fdf55311e7bca59c7ad2c70
Kholid Anas Amrulloh <amrulloh30@gmail.com>
2023-01-30 23:36:24 +0700

    add  fitur auth login user

---
+++ app/Models/Master/MCity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Master;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MCity extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+    protected $table = 'm_city';
+}
